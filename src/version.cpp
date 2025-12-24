54e12a8a741ef4017cf000e1d302a2ac0fa1588a
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-12-24 23:03:45 +0500

    fix: Add version.cpp to .gitignore

--- src/version.cpp
+++
@@ -1,8 +0,0 @@
-#include "version.hpp"
-
-namespace big
-{
-	const char* version::GIT_SHA1           = "c520c91ae83cdccb6f67d047efc4b6c5ad8d6443-dirty";
-	const char* version::GIT_BRANCH         = "master";
-	const char* version::GIT_DATE           = "Wed Dec 24 22:31:13 2025";
-};
